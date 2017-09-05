ddc805b6fab2f55c410cc9cff41f19c0eedb24ea
Lukey <lshort2@mail.csuchico.edu>
2017-09-04 19:56:12 -0700

    ship follows cursor and shoots at any clicked area

---
+++ README.md
@@ -0,0 +1,16 @@
+#Imperial Nav Destroyer
+##Because we're tired of all these old-looking websites.
+
+![alt text](https://im.ezgif.com/tmp/ezgif-1-ae981aab23.gif)
+
+##What is this?
+- This is a tool I created in my freetime to overlay the [Imperial Star Destroyer](http://starwars.wikia.com/wiki/Imperial_I-class_Star_Destroyer) on top of a webpage. For fun.
+- This a code repository that is provided as-is without any warranty whatsoever.
+
+##Have suggestions?
+- Let me know if you have any cool ways to expand on this. I am always eager to connect with people passionate about Star Wars and technology.
+
+##Attribution
+- Credit to [W3 schools](https://www.w3schools.com/) for their SUPER useful website. Please don't sue me for using your website in an example.
+- [wget](https://linux.die.net/man/1/wget) used to download webpage (see my [dotfiles repository](https://github.com/lshort2/dotFiles/blob/master/bash_aliases) for easier alias, "dlpage")
+- Credit to [ezgif](https://ezgif.com/) for creating gif from screen recording.
--- js/destroyer2.js
+++ js/destroyer2.js
@@ -1,68 +1,93 @@
-//Initialize all ship sounds in their respective variables
-var sound_fire = new Audio();
-sound_fire.src = "audio/ship-laser.mp3";
-var sound_hit = new Audio();
-sound_hit.src = "./audio/explosion.mp3";
-
-var HIT_DELAY = 2000;//Amount of time required to complete hit animation, before changing page
 console.log("File is loaded.");
-//var ship = document.getElementById("imperialNavDestroyer");
-//ship.setAttribute("style", "position:fixed; top:80%; left:20px; width: 360px;");
+
+//Initialize all ship sounds in their respective variables
+var sound_hit = new Audio();
+sound_hit.src = "../audio/explosion-3.wav";
+var HIT_DELAY = 2000;//Amount of time required to complete hit animation, before changing page
+
+
 var ship = $("#imperialNavDestroyer2");
 ship.css({"position":"fixed", "top":"82%", "left":"30px", "width": "320px", "z-index": "200"});
+
+//Global value to save angle of ship
+var saved_angle = 0;
+$(document).click(function (e) {
+  console.log("CLiK");
+  e.preventDefault();//make sure we get to shoot first...
+
+  //Log info
+  var target = e.target;
+  console.log("target: ", target);
+  console.log("event: ", e);
+  console.log("target.href: ", target.href);
+
+  var projectile = $('<img src="../img/laserbeam.png">');   // Create with jQuery
+  projectile.css({"position":"fixed", "top":"82%", "left":"42px", "width": "150px", "z-index": "199"});
+  projectile.css({ "-webkit-transform": 'rotate(' + (saved_angle-45) + 'deg)'});    
+  projectile.css({ "-webkit-transition": "0.4s", "transition": "0.4s"});    
+  projectile.css({ "transition-timing-function": "linear", "-webkit-transition-timing-function": "linear"});    
+
+  //projectile.css({ '-moz-transform': 'rotate(' + saved_angle+115 + 'deg)'});
+  //projectile.css({ 'transform': 'rotate(' + saved_angle+115 + 'deg)'});
+  $("body").append(projectile);
+  setTimeout(function(){ projectile.css({"top": (e.pageY-75)+"px", "left": (e.pageX-75)+"px"}); }, 10);
+  setTimeout(function(){ projectile.remove() }, 500);
+
+  var explosion = $('<img src="../img/explosion.gif">');   // Create with jQuery
+  explosion.css({"position":"fixed", "top": (e.pageY-60)+"px", "left": (e.pageX-40)+"px", "width": "100px", "z-index": "199", "display": "none"});
+
+  setTimeout(function(){ sound_hit.play(); }, 540);
+  setTimeout(function(){ explosion.css({"display": "inline"}); }, 600);
+  setTimeout(function(){ explosion.remove(); }, 740);
+
+
+
+
+
+
+  $("body").append(explosion);
+  setTimeout(function(){ projectile.remove() }, 500);
+  //setTimeout(function(){ projectile.css({"top":"42px", "left":"42px"}); }, 3000);
+  
+  //Check if we have to follow link
+  if(target.href) {
+    console.log("relocating");
+    console.log("3 seconds must pass.");
+    setTimeout(function(){ window.location = target.href; }, 1400);
+    //setTimeout(function(){ window.location = target.href; }, 3000);
+  }
+  else {
+    console.log("no href here");
+  }
+
+  //All done.
+  console.log("CLiK'd");
+});
 
 
 $(document).mousemove(function(e){
   //Dummy ball follows pointer
-  $("#ball").css({left:e.pageX-150, top:e.pageY-150});
+  //@@@ SUPER AWESOME PINK BALL
+  //$("#ball").css({left:e.pageX-150, top:e.pageY-150});
 
-  //Calculate x,y of ship itself
-  ////var shipoffset = ship.offset();
-  ////var ship_x = shipoffset.left;
-  ////var ship_y = shipoffset.top;
-  ////ship_center_x = ship_x + 180;
-  ////ship_center_y = ship_y + 101;
-  //320x124
   var top_shipOffset = 90;
   var left_shipOffset = 80;
-  var ship_center_x = ship.offset().left+ship.width()/2;
-  var ship_center_y = ship.offset().top+ship.height()/2;
-  //shipCenter = [ship.offset().left+ship.width()/2, ship.offset().top+ship.height()/2];
-  shipCenter = [ship.offset().left+80, ship.offset().top+80];
+  shipCenter = [ship.offset().left+left_shipOffset, ship.offset().top+top_shipOffset];
+  var a = e.pageX - shipCenter[0];
+  var b = e.pageY - shipCenter[1];
+  var dist = Math.sqrt(a*a + b*b);
 
-  //Calculate angle for ship to be pointed
-  //var angle = Math.atan2(e.pageX- ship_center_x,- (e.pageY- ship_center_y) )*(180/Math.PI);
-  var angle = Math.atan2(e.pageX- shipCenter[0],- (e.pageY- shipCenter[1]) )*(180/Math.PI);
-  angle -= 93;
-  ship.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});    
-  ship.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
-  ship.css({ 'transform': 'rotate(' + angle + 'deg)'});
-  ship.css({ 'transform-origin': "80px 80px"});
-  ship.css({ '-ms-transform-origin': "80px 80px"});
+  if(dist > 160) {
+    //Calculate angle for ship to be pointed
+    //var angle = Math.atan2(e.pageX- ship_center_x,- (e.pageY- ship_center_y) )*(180/Math.PI);
+    var angle = Math.atan2(e.pageX- shipCenter[0],- (e.pageY- shipCenter[1]) )*(180/Math.PI);
+    angle -= 94;
+    ship.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});    
+    ship.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
+    ship.css({ 'transform': 'rotate(' + angle + 'deg)'});
+    ship.css({ 'transform-origin': "80px 90px"});
+    ship.css({ '-ms-transform-origin': "80px 90px"});
 
-
-
-
-  //ship.css({ 'transform-origin': left_shipOffset + top_shipOffset});
-  //ship.css({ '-ms-transform-origin': left_shipOffset + top_shipOffset});
-  /*
-  ship.css({ 'transform-origin': ship.width()/2 + ship.height()/2});
-  ship.css({ '-ms-transform-origin': ship.width()/2 + ship.height()/2});
-  console.log("ship_x", ship_x);
-  console.log("ship_y", ship_y);
-  console.log("e.clientX", e.clientX);
-  console.log("e.clientY", e.clientY);
-  var top_ship2Offset = 74;
-  var left_ship2Offset = 60;
-  ship2_center_x = ship2.offset().left+ship2.width()/2+left_ship2Offset;
-  ship2_center_y = ship2.offset().top+ship2.height()/2+top_ship2Offset;
-
-  var angle2 = Math.atan2(e.pageX- ship2_center_x,- (e.pageY- ship2_center_y) )*(180/Math.PI);
-  angle2 -= 90;
-  ship2.css({ "-webkit-transform": 'rotate(' + angle2 + 'deg)'});    
-  ship2.css({ '-moz-transform': 'rotate(' + angle2 + 'deg)'});
-  ship2.css({ 'transform': 'rotate(' + angle2 + 'deg)'});
-  ship2.css({ 'transform-origin':  left_ship2Offset + ' ' + top_ship2Offset});
-  ship2.css({ '-ms-transform-origin': left_ship2Offset + ' ' + top_ship2Offset});
-  */
+    saved_angle = angle;
+  }
 });
